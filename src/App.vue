307053730c3a52259b28d5318aff945c3db4776f
faiz1503 <faiz16ti@mahasiswa.pcr.ac.id>
2021-01-06 00:07:31 +0700

    perubahan dan penambahan untuk todolist

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,87 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/profile">Profile</router-link> |
-    <router-link to="/about">About</router-link> |
+  <div id="app">
+
+    <div class="container">
+      <div class="row header mb-5 p-3">
+        <div class="col-lg-12">
+          <h2 class="">Apa yang saya Lakukan Hari Ini</h2>
+        </div>
+      </div>
+
+      <div class="row content">
+        <div class="col-lg-12">
+          <div class="pekerjaan-rumah">
+            <div class="row">
+              <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="mb-3 tugas col-lg-12 text-left">
+                <ItemTugas1 v-bind:tugasSaya="tugasSaya" />
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
   </div>
-  <router-view/>
 </template>
 
-<style>
+<script>
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import ItemTugas1 from "./components/ItemTugas1.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      tugas: [
+        {
+          id: 1,
+          namaTugas: "Belajar",
+          isDone: false,
+        },
+        {
+          id: 2,
+          namaTugas: "Bermain",
+          isDone: false,
+        },
+        {
+          id: 3,
+          namaTugas: "Tidur",
+          isDone: false,
+        },
+      ],
+    };
+  },
+  components: {
+    ItemTugas1
+  }
+};
+</script>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+}
+.header {
+  background: #1abc9c;
 }
 
-#nav {
-  padding: 30px;
+.header h2 {
+  margin: 0;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.tugas .item {
+  font-size: 20px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.item-completed {
+  color: #1abc9c;
 }
 </style>
---
+++ src/components/ItemTugas1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="form-check">
+      <input
+        class="form-check-input"
+        onClick=""
+        type="checkbox"
+        value=""
+        id="defaultCheck1"/>
+      <label
+        class="form-check-label item"
+        v-bind:class="{ 'item-completed': tugasSaya.isDone }"
+        for="defaultCheck1">
+        {{ tugasSaya.namaTugas }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemTugas1",
+  props: ["tugasSaya"],
+};
+</script>
+
+<style scoped>
+</style>
