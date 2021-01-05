c67e4e09a5c025c7dd5ffc0bb6e8e3cb35a5b2c4
faiz1503 <faiz16ti@mahasiswa.pcr.ac.id>
2021-01-05 22:26:17 +0700

    penambahan router halaman profil

---
+++ src/views/Profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="profile">
+    <h1>This is a Profile page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Profile',
+  components: {
+  }
+}
+</script>
