c9e00cbc27f83ca8a592b6afa155e91d5f3cc800
faiz1503 <faiz16ti@mahasiswa.pcr.ac.id>
2021-01-05 22:56:17 +0700

    penambahan pada profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,14 +1,40 @@
 <template>
   <div class="profile">
-    <h1>This is a Profile page</h1>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="media text-left">
+            <img src="../assets/profile.jpg" class="user-pic mr-3" />
+            <div class="media-body">
+              <h5 class="mt-0">Faiz Keren</h5>
+              <p>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+                nisi vulputate fringilla. Donec lacinia congue felis in
+                faucibus.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import "bootstrap/dist/css/bootstrap.css";
 
 export default {
-  name: 'Profile',
-  components: {
-  }
-}
+  name: "Profile",
+  components: {},
+};
 </script>
+
+<style scoped>
+.user-pic {
+    border-radius: 200%;
+    height: 100px;
+    width: 100px;
+}
+</style>
